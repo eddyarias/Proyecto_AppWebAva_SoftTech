2221b0efcc20ee9fb54270a1e0c35d0597bc9fdf
EDDY SANTIAGO ARIAS SARANGO <eddy.arias@epn.edu.ec>
2025-06-11 08:15:37 -0500

    funcionalidad: integración chat

---
+++ Codigo/WebAppGaleriaArte/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebAppGaleriaArte.Startup))]
+
+
+namespace WebAppGaleriaArte
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
