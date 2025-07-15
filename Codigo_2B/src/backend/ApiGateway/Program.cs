5a3285032b1d20dcff7bf7a8129d89010b7083db
Marco Antonio Marquez Santos <94476065+MarcoMarquez19@users.noreply.github.com>
2025-07-15 06:01:24 +0000

    fix apigateway and first test to ensure comunicate between front-back

--- Codigo_2B/src/backend/ApiGateway/Program.cs
+++ Codigo_2B/src/backend/ApiGateway/Program.cs
@@ -1,9 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Configuración de CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
+
+// Usar CORS
+app.UseCors("AllowAll");
 
 app.MapReverseProxy();
 
