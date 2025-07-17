a40fcc1768da4a5ba78e2e7cc4bde4d4c8224510
Marco Antonio Marquez Santos <94476065+MarcoMarquez19@users.noreply.github.com>
2025-07-17 17:52:38 +0000

    Add Port configuration obraservice and jwt implementation

--- Codigo_2B/src/backend/services/ObraService/GaleriaArte.ObraService.API/Controllers/TestController.cs
+++ Codigo_2B/src/backend/services/ObraService/GaleriaArte.ObraService.API/Controllers/TestController.cs
@@ -1,12 +1,24 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace GaleriaArte.ObraService.API.Controllers
+namespace GaleriaArte.ObraService.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ObrasController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class HealthController : ControllerBase
+    [HttpGet("publicas")]
+    [AllowAnonymous]
+    public IActionResult GetObrasPublicas()
     {
-        [HttpGet]
-        public IActionResult Get() => Ok("MediaService is running.");
+        return Ok(new { mensaje = "Esto es público" });
+    }
+
+    [HttpGet("protegidas")]
+    [Authorize]
+    public IActionResult GetObrasProtegidas()
+    {
+        var correo = User.Claims.FirstOrDefault(c => c.Type == "email")?.Value;
+        return Ok(new { mensaje = "Autenticado correctamente", usuario = correo });
     }
 }
